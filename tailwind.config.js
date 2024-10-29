926bfb406c9598ce6842cd7c56eaf4cfa49d1096
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-10-30 02:38:58 +0530

    Mid insurance services layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
 	content: ["./src/**/*.{html,ts}"],
 	theme: {
 		extend: {
+			spacing: {
+				88: "22rem",
+			},
 			colors: {
 				accent: "#138f82",
 				"accent-light": "#dff0ef",
